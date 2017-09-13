64da17edb46d4e000a3ca388cd224376dd1df787
dkniazevych <dkniazevych@hiqinvest.nl>
2017-09-13 14:59:37 +0200

    remove lib/ folder from Git

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as file2html from 'file2html';
-export default class TextReader extends file2html.Reader {
-    read({fileInfo}: file2html.ReaderParams): Promise<file2html.File>;
-    static testFileMimeType(mimeType: string): boolean;
-}
